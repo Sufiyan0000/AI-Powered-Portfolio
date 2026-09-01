623ddb982ae05668d4e162374faa120a78af11a2
Sufiyan0000 <alimdsufiyan89@gmail.com>
2026-09-01 12:52:54 +0530

    fix the bug and optimize the main.py

--- backend/apps/main.py
+++ backend/apps/main.py
@@ -3,29 +3,34 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from apps.api.chat import router as chat_router
 from apps.api.admin import router as admin_router
+from apps.core.config import settings
+
 
 app = FastAPI(
-    title="AI-Powered Portfolio API",
-    version= '1.0.0'
+    title=settings.APP_NAME,
+    version="1.0.0",
+    debug=settings.DEBUG,
 )
 
-## CORS-MIDDLEWARE
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000','http:127.0.0.1:3000'],
-    allow_credentials= True,
-    allow_methods= ['*'],
-    allow_headers= ['*']
+    allow_origins=settings.cors_origins_list,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type", "Authorization"],
 )
+
 
 app.include_router(
     chat_router,
-    prefix='/api/chat',
-    tags=['Chat']
+    prefix="/api/chat",
+    tags=["Chat"],
 )
+
 
 app.include_router(
     admin_router,
-    prefix='/admin',
-    tags=['admin']
+    prefix="/admin",
+    tags=["Admin"],
 )
